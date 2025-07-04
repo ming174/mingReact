675fc1e43d8f8c383c3ad67f6a09238a59fe3984
ming174 <imy82174@gmail.com>
2025-07-04 17:09:08 +0900

    연습

---
+++ frontend/CH2/src/router/todoRouter.tsx
@@ -0,0 +1,14 @@
+import { lazy, Suspense } from "react"
+
+const Loading = () => <div>Loading....</div>
+const TodoIndex = lazy(() => import("../pages/todo/indexPage"))
+
+const todoRouter = () => {
+    
+    return ({
+        path: 'todo',
+        element: <Suspense fallback={<Loading/>}><TodoIndex/></Suspense>
+    })
+}     
+
+export default todoRouter
