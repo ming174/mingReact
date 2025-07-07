2f4ea84f7b7156e3e1a67699dd28b04661ef60fb
ming174 <imy82174@gmail.com>
2025-07-07 17:28:51 +0900

    0707

---
+++ backend/apiserver/src/main/java/org/zerock/apiserver/domain/Todo.java
@@ -0,0 +1,39 @@
+package org.zerock.apiserver.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "tbl_todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    private String content;
+
+    private boolean complete;
+
+    private LocalDate dueDate;
+}
---
+++ backend/apiserver/src/test/java/org/zerock/apiserver/repository/TodoRepositoryTests.java
@@ -0,0 +1,24 @@
+package org.zerock.apiserver.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class TodoRepositoryTests {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test 
+    public void test1() {
+
+        log.info(todoRepository.getClass().getName());
+
+
+    }
+}
+ 
